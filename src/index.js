cb99f479c2151e57de33292cab5f4a3a3d1174e6
JamieG <yoshijay3000@hotmail.com>
2020-12-03 22:35:18 +0800

    handled submit event for signup modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
+const signupForm = document.querySelector('#signup-form');
+const signupEmail = document.querySelector('#signupEmail')
+const signupPassword = document.querySelector('#signupPassword');
+const signupFname = document.querySelector('#signupFname');
+const signupLname = document.querySelector('#signupLname');
+const signupGender = document.querySelector('input[name="signupGender"]');
+const signupBday = document.querySelector('#signupBday');
+const signupSchoolName = document.querySelector('#signupSchoolName');
+const signupSchoolType = document.querySelector('#signupSchoolType');
 
+
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    fetch('http://localhost:3333/signup', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            signupEmail: signupEmail.value,
+            signupPassword: signupPassword.value,
+            signupFname: signupFname.value,
+            signupLname: signupLname.value,
+            signupGender: signupGender.value,
+            signupBday: signupBday.value,
+            signupSchoolName: signupSchoolName.value,
+            signupSchoolType: signupSchoolType.value
+        })
+    }).then(response => {
+        console.log(response);
+    })
+        .catch(err => console.log(err));
+});
